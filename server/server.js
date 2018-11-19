91db112314f21efead893bbdbb1849307f09fa4e
Bert De Backer <BertDeBackerkonikx@gmail.com>
2018-11-19 11:14:44 +0100

    correct bug process.end.PORT to process.env.PORT

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const publicPath = path.join(__dirname, '../public')
-const post = process.end.PORT || 3000
+const post = process.env.PORT || 3000
 
 const app = express()
 app.use(express.static(publicPath));
